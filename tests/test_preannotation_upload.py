48699d08486d9d13fc9ef3c1ec36e1150c989407
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-08-27 11:45:12 +0400

    Preannotation test

--- tests/test_preannotation_upload.py
+++ tests/test_preannotation_upload.py
@@ -10,11 +10,11 @@ sa.init(Path.home() / ".superannotate" / "config.json")
 @pytest.mark.parametrize(
     "project_type,name,description,from_folder", [
         (
-            1, "Example Project test", "test vector",
+            1, "Example Project test vector", "test vector",
             Path("./tests/sample_project_vector")
         ),
         (
-            2, "Example Project test", "test pixel",
+            2, "Example Project test pixel", "test pixel",
             Path("./tests/sample_project_pixel")
         ),
     ]
@@ -22,8 +22,6 @@ sa.init(Path.home() / ".superannotate" / "config.json")
 def test_preannotation_folder_upload_download(
     project_type, name, description, from_folder, tmpdir
 ):
-    if project_type == 2:  # skip until implemented
-        return
     team = sa.get_default_team()
     projects_found = sa.search_projects(team, name)
     for pr in projects_found:
@@ -33,9 +31,16 @@ def test_preannotation_folder_upload_download(
     sa.upload_images_from_folder_to_project(
         project, from_folder, annotation_status=2
     )
-    sa.upload_preannotations_from_folder_to_project(project, from_folder)
+    old_to_new_classid_conversion = sa.create_annotation_classes_from_classes_json(
+        project, from_folder / "classes" / "classes.json"
+    )
+    sa.upload_preannotations_from_folder_to_project(
+        project, from_folder, old_to_new_classid_conversion
+    )
     count_in = len(list(from_folder.glob("*.json")))
 
+    if project_type == 2:  # skip until implemented
+        return
     images = sa.search_images(project)
     for image in images:
         sa.download_image_preannotations(image, tmpdir)
--- tests/test_single_annotation_upload_download.py
+++ tests/test_single_annotation_upload_download.py
@@ -13,11 +13,11 @@ PROJECT_NAME = "testing_1419"
 @pytest.mark.parametrize(
     "project_type,name,description,from_folder", [
         (
-            1, "Example Project test", "test vector",
+            1, "Example Project test vector", "test vector",
             Path("./tests/sample_project_vector")
         ),
         (
-            2, "Example Project test", "test pixe",
+            2, "Example Project test pixel", "test pixel",
             Path("./tests/sample_project_pixel")
         )
     ]
@@ -32,9 +32,6 @@ def test_annotation_download_upload(
     project = sa.create_project(team, name, description, project_type)
     sa.upload_images_from_folder_to_project(
         project, from_folder, annotation_status=1
-    )
-    sa.create_annotation_classes_from_classes_json(
-        project, from_folder / "classes" / "classes.json"
     )
     sa.upload_annotations_from_folder_to_project(project, from_folder)
     image = sa.search_images(project)[3]
@@ -54,4 +51,43 @@ def test_annotation_download_upload(
         assert filecmp.cmp(
             input_annotation_paths[1], anns_mask_in_folder[0], shallow=False
         )
+    # sa.download_image_preannotations(image, tmpdir)
+
+
+@pytest.mark.parametrize(
+    "project_type,name,description,from_folder", [
+        (
+            1, "Example Project test vector cc", "test vector",
+            Path("./tests/sample_project_vector")
+        ),
+        (
+            2, "Example Project test pixel cc", "test pixel",
+            Path("./tests/sample_project_pixel")
+        )
+    ]
+)
+def test_annotation_download_upload_withclassesconversion(
+    project_type, name, description, from_folder, tmpdir
+):
+    team = sa.get_default_team()
+    projects = sa.search_projects(team, name)
+    for project in projects:
+        sa.delete_project(project)
+    project = sa.create_project(team, name, description, project_type)
+    sa.upload_images_from_folder_to_project(
+        project, from_folder, annotation_status=1
+    )
+    old_to_new_classid_conversion = sa.create_annotation_classes_from_classes_json(
+        project, from_folder / "classes" / "classes.json"
+    )
+    sa.upload_annotations_from_folder_to_project(
+        project, from_folder, old_to_new_classid_conversion
+    )
+    image = sa.search_images(project)[3]
+    sa.download_image_annotations(image, tmpdir)
+    anns_json_in_folder = list(Path(tmpdir).glob("*.json"))
+    anns_mask_in_folder = list(Path(tmpdir).glob("*.png"))
+    assert len(anns_json_in_folder) == 1
+    assert len(anns_mask_in_folder) == project_type - 1
+
     # sa.download_image_preannotations(image, tmpdir)
