043c073421584f41e25f4bd16d27971ef52dad8c
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-09-03 18:39:45 +0400

    Version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ packages = find_packages()
 
 setup(
     name='superannotate',
-    version='0.2.2',
+    version='0.2.3',
     description='Python SDK to SuperAnnotate platform',
     license='MIT',
     author='Hovnatan Karapetyan',
@@ -25,8 +25,8 @@ setup(
     install_requires=requirements,
     setup_requires=['wheel'],
     packages=find_packages(exclude=('tests', )),
-    entry_points={
-        'console_scripts': ['superannotate = superannotate.__main__:main']
-    },
+    # entry_points={
+    #     'console_scripts': ['superannotate = superannotate.__main__:main']
+    # },
     python_requires='>=3.5'
 )
