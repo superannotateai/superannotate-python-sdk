73ab19af6a1922e3a7fcbd1d1bcedef79da758fc
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-09-23 18:26:50 +0400

    Version bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ packages = find_packages()
 
 setup(
     name='superannotate',
-    version='1.9.1',
+    version='1.9.2',
     description='Python SDK to SuperAnnotate platform',
     license='MIT',
     author='Hovnatan Karapetyan',
