0e4348b476e50e0c304093fde1e6d996aacc25d0
rcmanga <r.cmangasaryan@gmail.com>
2020-10-02 17:23:21 +0400

    add columns image_name, point_labels, meta, probability

--- superannotate/analytics/common.py
+++ superannotate/analytics/common.py
@@ -6,77 +6,98 @@ import logging
 logger = logging.getLogger("superannotate-python-sdk")
 
 
-def aggregate_annotations_as_df(export_root, project_names):
-    """Aggregate annotations as pandas dataframe across multiple projects.
-    :param export_root: root export path of the projects
-    :type export_root: Pathlike (str or Path)
-    :param project_names: list of project names to aggregate through
-    :type project_names: list of str
-    :return: DataFrame on annotations with columns: ["class", "attribute_group", "attribute_name", "type", "error"] 
+def aggregate_annotations_as_df(project_root):
+    """Aggregate annotations as pandas dataframe from project root.
+    :param project_root: export path of the project
+    :type project_root: Pathlike (str or Path)
+    :return: DataFrame on annotations with columns: ["image_name", class", "attribute_group", "attribute_name", "type", "error", "probability", "point_labels", "meta"] 
     :rtype: pandas DataFrame
     """
 
-    logger.info(
-        "Aggregating annotations as pandas dataframe accross projects: {}.".format(
-            ' '.join(project_names)
-        ),
-    )
+    logger.info("Aggregating annotations as pandas dataframe")
 
     annotation_data = {
+        "image_name": [],
         "class": [],
         "attribute_group": [],
         "attribute_name": [],
         "type": [],
-        "error": []
+        "error": [],
+        "probability": [],
+        "point_labels": [],
+        "meta": []
     }
 
-    for project_name in project_names:
-        project_root = Path(export_root).joinpath(project_name)
+    annotations_paths = []
 
-        annotations_paths = []
+    for path in Path(project_root).glob('*.json'):
+        if path.name.endswith('___objects.json'
+                             ) or path.name.endswith('___pixel.json'):
+            annotations_paths.append(path)
 
-        for path in Path(project_root).glob('*.json'):
-            if path.name.endswith('___objects.json'
-                                 ) or path.name.endswith('___pixel.json'):
-                annotations_paths.append(path)
+    for annotation_path in annotations_paths:
+        annotation_json = json.load(open(annotation_path))
+        vector = annotation_path.name.endswith('___objects.json')
+        annotation_image_name = annotation_path.name.split("___")[0]
 
-        for annotation_path in annotations_paths:
-            annotation_json = json.load(open(annotation_path))
+        for annotation in annotation_json:
+            if 'className' not in annotation:
+                continue
 
-            for annotation in annotation_json:
-                if 'className' not in annotation:
-                    continue
+            annotation_class_name = annotation["className"]
 
-                class_name = annotation["className"]
+            annotation_type = annotation['type'] if vector else "mask"
 
-                annotation_type = annotation['type'
-                                            ] if annotation_path.name.endswith(
-                                                '___objects.json'
-                                            ) else "mask"
-                error = annotation['error'] if 'error' in annotation else None
+            if annotation_type in ["bbox", "polygon", "polyline", "cuboid"]:
+                annotation_meta = {"points": annotation["points"]}
 
-                attributes = annotation["attributes"]
-                if not attributes:
+            if annotation_type == "point":
+                annotation_meta = {"x": annotation["x"], "y": annotation["y"]}
+            if annotation_type == "ellipse":
+                annotation_meta = {
+                    "cx": annotation["cx"],
+                    "cy": annotation["cy"],
+                    "rx": annotation["rx"],
+                    "ry": annotation["ry"],
+                    "angle": annotation["angle"]
+                }
 
-                    annotation_data["class"].append(class_name)
-                    annotation_data["attribute_group"].append(None)
-                    annotation_data["attribute_name"].append(None)
+            if annotation_type == "mask":
+                annotation_meta = {"parts": annotation["parts"]}
 
-                    annotation_data["type"].append(annotation_type)
-                    annotation_data["error"].append(error)
+            annotation_error = annotation['error'
+                                         ] if 'error' in annotation else None
+            annotation_probability = annotation["probability"]
 
-                for attribute in attributes:
+            annotation_point_labels = annotation["pointLabels"
+                                                ] if vector else None
 
-                    attribute_group = attribute["groupName"]
-                    attribute_name = attribute['name']
+            attributes = annotation["attributes"]
+            if not attributes:
+                annotation_data["image_name"].append(annotation_image_name)
+                annotation_data["class"].append(annotation_class_name)
+                annotation_data["attribute_group"].append(None)
+                annotation_data["attribute_name"].append(None)
+                annotation_data["type"].append(annotation_type)
+                annotation_data["meta"].append(annotation_meta)
+                annotation_data["error"].append(annotation_error)
+                annotation_data["probability"].append(annotation_probability)
+                annotation_data["point_labels"].append(annotation_point_labels)
 
-                    annotation_data["class"].append(class_name)
+            for attribute in attributes:
 
-                    annotation_data["attribute_group"].append(attribute_group)
-                    annotation_data["attribute_name"].append(attribute_name)
+                attribute_group = attribute["groupName"]
+                attribute_name = attribute['name']
 
-                    annotation_data["type"].append(annotation_type)
-                    annotation_data["error"].append(error)
+                annotation_data["image_name"].append(annotation_image_name)
+                annotation_data["class"].append(annotation_class_name)
+                annotation_data["attribute_group"].append(attribute_group)
+                annotation_data["attribute_name"].append(attribute_name)
+                annotation_data["type"].append(annotation_type)
+                annotation_data["meta"].append(annotation_meta)
+                annotation_data["error"].append(annotation_error)
+                annotation_data["probability"].append(annotation_probability)
+                annotation_data["point_labels"].append(annotation_point_labels)
 
     df = pd.DataFrame(annotation_data)
 
