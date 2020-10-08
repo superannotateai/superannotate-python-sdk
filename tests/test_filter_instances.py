972e5458470068f3b4c5bfc73b3fe53eab863cf3
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-10-08 14:30:10 +0400

    Progress on instance filtering

--- tests/test_filter_instances.py
+++ tests/test_filter_instances.py
@@ -7,6 +7,8 @@ import superannotate as sa
 
 PROJECT_NAME_1 = "test filter instances"
 
+PROJECT_DIR = "./tests/sample_project_vector"
+
 
 def test_filter_instances(tmpdir):
     tmpdir = Path(tmpdir)
@@ -17,12 +19,10 @@ def test_filter_instances(tmpdir):
 
     # project = sa.create_project(PROJECT_NAME_1, "test", "Vector")
 
-    not_filtered = sa.filter_annotation_instances(
-        "./tests/sample_project_vector"
-    )
+    not_filtered = sa.filter_annotation_instances(PROJECT_DIR)
 
     filtered_excl = sa.filter_annotation_instances(
-        "./tests/sample_project_vector",
+        PROJECT_DIR,
         exclude=[{
             "className": "Personal vehicle"
         }, {
@@ -31,8 +31,7 @@ def test_filter_instances(tmpdir):
     )
 
     filtered_incl = sa.filter_annotation_instances(
-        "./tests/sample_project_vector",
-        include=[{
+        PROJECT_DIR, include=[{
             "className": "Large vehicle"
         }]
     )
@@ -40,7 +39,7 @@ def test_filter_instances(tmpdir):
     assert filtered_incl.equals(filtered_excl)
 
     all_filtered = sa.filter_annotation_instances(
-        "./tests/sample_project_vector", include=[{
+        PROJECT_DIR, include=[{
             "className": "bogus"
         }]
     )
@@ -50,9 +49,30 @@ def test_filter_instances(tmpdir):
     vc = not_filtered["type"].value_counts()
     for i in vc.index:
         all_filtered = sa.filter_annotation_instances(
-            "./tests/sample_project_vector", include=[{
+            PROJECT_DIR, include=[{
                 "type": i
             }]
         )
 
         assert len(all_filtered) == vc[i]
+
+    vcc = not_filtered["class"].value_counts()
+    for i in vcc.index:
+        if len(not_filtered[not_filtered["class"] == i]["type"].unique()) > 1:
+            break
+
+    vcc_different_types = not_filtered[not_filtered["class"] == i
+                                      ]["type"].value_counts()
+
+    t_c = sa.filter_annotation_instances(
+        PROJECT_DIR,
+        include=[{
+            "className": i,
+            "type": vcc_different_types.index[0]
+        }]
+    )
+    assert len(t_c) == len(
+        not_filtered[(not_filtered["type"] == vcc_different_types.index[0]) &
+                     (not_filtered["class"] == i)]
+    )
+    # print(not_filtered[not_filtered["className"] == "Human
