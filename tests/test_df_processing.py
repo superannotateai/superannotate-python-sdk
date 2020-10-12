3905fcd14aee9a4b3694703e66942a78d0941ed5
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-10-12 18:14:52 +0400

    Df generation test

---
+++ tests/test_df_processing.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+import pytest
+
+import superannotate as sa
+
+PROJECT_NAME_1 = "test df processing"
+
+PROJECT_DIR = "./tests/sample_project_vector"
+
+
+def test_filter_instances(tmpdir):
+    tmpdir = Path(tmpdir)
+
+    # projects = sa.search_projects(PROJECT_NAME_1, return_metadata=True)
+    # for project in projects:
+    #     sa.delete_project(project)
+
+    # project = sa.create_project(PROJECT_NAME_1, "test", "Vector")
+
+    df = sa.aggregate_annotations_as_df(PROJECT_DIR)
+    df = df[~(df.duplicated(["instanceId", "imageName"]))]
+    df = df[df.duplicated(["trackingId"], False) & df["trackingId"].notnull()]
+    assert len(df) == 2
+    assert df.iloc[0]["imageName"] == "example_image_1.jpg"
+    assert df.iloc[1]["imageName"] == "example_image_2.jpg"
