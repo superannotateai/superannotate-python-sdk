4c4f1083c736b016f8201a64b8c56e98969a5d0a
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-10-22 10:08:30 +0400

    venv python3.6

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-python -m venv venv_sa_conv
+# sudo add-apt-repository ppa:deadsnakes/ppa
+# sudo apt update
+# sudo apt install python3.6 python3.6-venv
+
+rm -rf venv_sa_conv
+python3.6 -m venv venv_sa_conv
 source venv_sa_conv/bin/activate
 pip install numpy # this is to avoid pycocotools incorrect dependency resolution issue
 pip install -r requirements.txt
