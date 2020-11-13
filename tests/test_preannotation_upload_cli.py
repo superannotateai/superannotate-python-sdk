d04a676be8876f9a1ffdf0ef7e1cf5409c0b7833
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-11-13 15:05:46 +0400

    Annotation CLI upload test update

--- tests/test_preannotation_upload_cli.py
+++ tests/test_preannotation_upload_cli.py
@@ -52,6 +52,44 @@ def test_preannotation_folder_upload_download_cli(
     assert count_in == count_out
 
 
+def test_annotation_folder_upload_download_cli_vector_COCO(tmpdir):
+    project_type = "Vector"
+    name = "Example Project test vector2 annotation cli upload coco vector"
+    description = "test"
+    from_folder = "./tests/converter_test/COCO/input/toSuperAnnotate/keypoint_detection"
+    task = "keypoint_detection"
+    dataset_name = "person_keypoints_test"
+
+    projects_found = sa.search_projects(name, return_metadata=True)
+    for pr in projects_found:
+        sa.delete_project(pr)
+
+    project = sa.create_project(name, description, project_type)
+    sa.upload_images_from_folder_to_project(
+        project, from_folder, annotation_status="InProgress"
+    )
+    # sa.create_annotation_classes_from_classes_json(
+    #     project, from_folder / "classes" / "classes.json"
+    # )
+    subprocess.run(
+        [
+            f"superannotate upload-annotations --project '{name}' --folder '{from_folder}' --format COCO --task {task} --dataset-name {dataset_name}"
+        ],
+        check=True,
+        shell=True
+    )
+    # time.sleep(5)
+    # count_in = len(list(from_folder.glob("*.json")))
+
+    # images = sa.search_images(project)
+    # for image_name in images:
+    #     sa.download_image_preannotations(project, image_name, tmpdir)
+
+    # count_out = len(list(Path(tmpdir).glob("*.json")))
+
+    # assert count_in == count_out
+
+
 def test_preannotation_folder_upload_download_cli_vector_COCO(tmpdir):
     project_type = "Vector"
     name = "Example Project test vector2 preannotation cli upload coco vector"
