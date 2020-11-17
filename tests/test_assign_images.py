33bdd72217f10fdb120d65cec27ad962ed1948e6
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-11-17 13:58:36 +0400

    Fix tests

--- tests/test_assign_images.py
+++ tests/test_assign_images.py
@@ -16,25 +16,25 @@ def test_assign_images(tmpdir):
         sa.delete_project(project)
 
     project = sa.create_project(PROJECT_NAME_VECTOR, "test", "Vector")
-    sa.share_project(project, "hovnatan@superannotate.com", "QA")
+    email = sa.get_team_metadata()["users"][0]["email"]
+    sa.share_project(project, email, "QA")
 
     sa.upload_images_from_folder_to_project(
         project, "./tests/sample_project_vector"
     )
 
     sa.assign_images(
-        project, ["example_image_1.jpg", "example_image_2.jpg"],
-        "hovnatan@superannotate.com"
+        project, ["example_image_1.jpg", "example_image_2.jpg"], email
     )
 
     time.sleep(1)
     im1_metadata = sa.get_image_metadata(project, "example_image_1.jpg")
     im2_metadata = sa.get_image_metadata(project, "example_image_2.jpg")
 
-    assert im1_metadata["qa_id"] == "hovnatan@superannotate.com"
-    assert im2_metadata["qa_id"] == "hovnatan@superannotate.com"
+    assert im1_metadata["qa_id"] == email
+    assert im2_metadata["qa_id"] == email
 
-    sa.unshare_project(project, "hovnatan@superannotate.com")
+    sa.unshare_project(project, email)
 
     time.sleep(1)
 
@@ -46,19 +46,17 @@ def test_assign_images(tmpdir):
     assert im1_metadata["annotator_id"] is None
     assert im2_metadata["annotator_id"] is None
 
-    sa.share_project(project, "hovnatan@superannotate.com", "Annotator")
+    sa.share_project(project, email, "Annotator")
 
     sa.assign_images(
-        project, ["example_image_1.jpg", "example_image_2.jpg"],
-        "hovnatan@superannotate.com"
+        project, ["example_image_1.jpg", "example_image_2.jpg"], email
     )
 
     time.sleep(1)
     im1_metadata = sa.get_image_metadata(project, "example_image_1.jpg")
     im2_metadata = sa.get_image_metadata(project, "example_image_2.jpg")
 
-    assert im1_metadata["annotator_id"] == "hovnatan@superannotate.com"
-    assert im2_metadata["annotator_id"] == "hovnatan@superannotate.com"
+    assert im1_metadata["annotator_id"] == email
+    assert im2_metadata["annotator_id"] == email
     assert im1_metadata["qa_id"] is None
     assert im2_metadata["qa_id"] is None
-    print(sa.get_project_metadata(PROJECT_NAME_VECTOR))
