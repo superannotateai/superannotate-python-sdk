3bf93c7cd2bd3afe802c13b8605b59ec281b4ece
rcmanga <r.cmangasaryan@gmail.com>
2020-11-25 01:14:57 +0400

    update docstring

--- superannotate/analytics/user_analytics.py
+++ superannotate/analytics/user_analytics.py
@@ -10,7 +10,7 @@ def image_analytics(annotations_df, visualize = False):
     :param visulaize: enables image analytics scatter plot
     :type visualize: bool
 
-    :return: DataFrame on image analytics with columns ["image", "instances", "time"]
+    :return: DataFrame on image analytics with columns ["userEmail", "userRole", "imageName", "annotationTime", "instanceCount"]
     :rtype: pandas DataFrame
 
     """
@@ -20,7 +20,7 @@ def image_analytics(annotations_df, visualize = False):
         grp.loc[grp_lost_msk] = grp[~grp_lost_msk].median()
         return grp
     
-    analytics = {"user_id": [], "user_role": [], "image": [], "time": [], "ninstances": [] }
+    analytics = {"userEmail": [], "userRole": [], "imageName": [], "annotationTime": [], "instanceCount": [] }
     annot_cols = ["imageName", "instanceId", "createdAt", "creatorEmail", "creatorRole"]
     annotations_df = annotations_df[annotations_df["creationType"] == "Manual"][annot_cols].drop_duplicates()
 
@@ -31,24 +31,24 @@ def image_analytics(annotations_df, visualize = False):
         img_time = grp.groupby("imageName", as_index=False)["time_spent"].agg("sum")
         img_n_instance = grp.groupby("imageName")["instanceId"].agg("count")
 
-        analytics["image"] += img_time.imageName.tolist()
-        analytics["time"] += img_time.time_spent.tolist()
-        analytics["ninstances"] += img_n_instance.tolist()
-        analytics["user_id"] += [annot[0]] * len(img_time)
-        analytics["user_role"] += [annot[1]] * len(img_time)
+        analytics["imageName"] += img_time.imageName.tolist()
+        analytics["annotationTime"] += img_time.time_spent.tolist()
+        analytics["instanceCount"] += img_n_instance.tolist()
+        analytics["userEmail"] += [annot[0]] * len(img_time)
+        analytics["userRole"] += [annot[1]] * len(img_time)
 
     analytics_df = pd.DataFrame(analytics)
     if visualize:
         #scatter plot of number of instances vs annotation time
         fig = px.scatter(
             analytics_df,
-            x="ninstances",
-            y="time",
-            color="user_id",
-            facet_col="user_role",
-            custom_data = ["image"],
+            x="instanceCount",
+            y="annotationTime",
+            color="userEmail",
+            facet_col="userRole",
+            custom_data = ["imageName"],
+            labels = {'userEmail': "User Email", "instanceCount": "Number of Instances", "annotationTime": "Annotation time"},
             color_discrete_sequence=px.colors.qualitative.Dark24,
-            labels = {'user_id': "User Email", "ninstances": "Number of Instances", "time": "Annotation time"}
         )
         fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
         fig.update_traces(hovertemplate="%{customdata[0]}")
