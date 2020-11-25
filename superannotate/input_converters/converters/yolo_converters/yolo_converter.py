bb4caef7dd42a10d5ff4c70ee41315f3ddda4b61
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-11-25 13:43:37 +0400

    Beta feature warning

--- superannotate/input_converters/converters/yolo_converters/yolo_converter.py
+++ superannotate/input_converters/converters/yolo_converters/yolo_converter.py
@@ -1,10 +1,16 @@
 from pathlib import Path
 import json
+import logging
 import time
 
+logger = logging.getLogger("superannotate-python-sdk")
 
-class YOLOConverter(object):
+
+class YOLOConverter():
     def __init__(self, args):
+        logger.warning(
+            "Beta feature. YOLO to SuperAnnotate annotation format converter is in BETA state."
+        )
         self.dataset_name = args.dataset_name
         self.project_type = args.project_type
         self.task = args.task
