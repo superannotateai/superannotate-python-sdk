023071fc804c1e159af5dc58357143ec825e075f
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-12-08 13:36:25 +0400

    Test stress upload fix

--- tests/test_stress_test_image_upload.py
+++ tests/test_stress_test_image_upload.py
@@ -5,7 +5,7 @@ import pytest
 
 import superannotate as sa
 
-sa.init(Path.home() / ".superannotate" / "config.json")
+PROJECT_NAME = "test stress upload"
 
 
 @pytest.mark.skipif(
@@ -13,8 +13,14 @@ sa.init(Path.home() / ".superannotate" / "config.json")
     os.environ["AO_TEST_LEVEL"] != "stress",
     reason="Requires env variable to be set"
 )
-def test_upload_stress():
-    project = sa.create_project("test_test_15", "hk", 1)
+def test_upload_stress(tmpdir):
+    tmpdir = Path(tmpdir)
+
+    projects = sa.search_projects(PROJECT_NAME, return_metadata=True)
+    for project in projects:
+        sa.delete_project(project)
+    project = sa.create_project(PROJECT_NAME, "hk", 1)
+
     sa.create_annotation_classes_from_classes_json(
         project, "tests/sample_project_vector/classes/classes.json"
     )
@@ -26,5 +32,3 @@ def test_upload_stress():
     )
     count = sa.get_project_image_count(project)
     assert count == 40670
-
-    # sa.delete_project(project)
