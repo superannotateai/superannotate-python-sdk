2d8e5f8feed3a4d382fd6a42f534970e595f5755
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-12-09 19:18:56 +0400

    Remove cocoapi dependency

--- install.sh
+++ install.sh
@@ -11,8 +11,10 @@ rm -rf venv_sa_conv
 python${PYTHON_VER} -m venv venv_sa_conv
 source venv_sa_conv/bin/activate
 
-pip install -e .
-# pip install superannotate
+pip install -r requirements.txt
+
+# pip install -e .
+pip install --pre superannotate
 
 # for testing
 pip install pytest pytest-xdist coverage pytest-cov pylint pylint-json2html pylint-pytest
