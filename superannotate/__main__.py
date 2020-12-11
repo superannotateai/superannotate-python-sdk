d4acc3d7e33d3bed799314358a94c07ecf8c72e5
agyurjin <agyurjin@gmail.com>
2020-12-11 16:07:00 +0400

    use extensions for cli

--- superannotate/__main__.py
+++ superannotate/__main__.py
@@ -9,6 +9,8 @@ import superannotate as sa
 
 from .exceptions import SABaseException
 
+import os
+
 logger = logging.getLogger("superannotate-python-sdk")
 
 _CLI_COMMAND = Path(sys.argv[0]).name
@@ -100,6 +102,16 @@ def preannotations_upload(command_name, args):
         help=
         'Task type for COCO projects can be panoptic_segmentation (Pixel), instance_segmentation (Pixel), instance_segmentation (Vector), keypoint_detection (Vector)'
     )
+    parser.add_argument(
+        '--extensions', required=False, help='List of image file extensions'
+    )
+
+    parser.add_argument(
+        '--image-dir',
+        required=False,
+        help='Path to images directory in input folder'
+    )
+
     args = parser.parse_args(args)
 
     if args.format != "SuperAnnotate":
@@ -115,6 +127,22 @@ def preannotations_upload(command_name, args):
             raise sa.SABaseException(
                 0, "Task name should be present for COCO format upload."
             )
+
+        extensions = []
+        if args.extensions is None:
+            extensions.append('jpg')
+        else:
+            exts = args.extensions.split(',')
+            for ext in exts:
+                extensions.append(ext)
+        logger.info(
+            "Images with the following will be used extension : %s. ",
+            extensions
+        )
+
+        if args.image_dir is None:
+            args.image_dir = ''
+
         logger.info("Annotations in format %s.", args.format)
         project_type = sa.get_project_metadata(args.project)["type"]
 
@@ -122,9 +150,10 @@ def preannotations_upload(command_name, args):
         tempdir_path = Path(tempdir.name)
         sa.import_annotation(
             args.folder, tempdir_path, "COCO", args.dataset_name, project_type,
-            args.task
+            args.task, 'Web', args.image_dir, extensions
         )
         args.folder = tempdir_path
+
     sa.create_annotation_classes_from_classes_json(
         args.project,
         Path(args.folder) / "classes" / "classes.json"
