759beb98415907e3574785bb06fe476709a83e15
Hovnatan Karapetyan <hovnatan@gmail.com>
2021-03-17 11:49:33 +0400

    Cleanup install dev script

--- install_dev_env.sh
+++ install_dev_env.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-PYTHON_VER="3.7"
+if [ "$1" == "" ]; then
+    PYTHON_VER="3"
+else
+    PYTHON_VER="$1"
+fi
 
 # install ubuntu dependencies
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update
-sudo apt install python${PYTHON_VER} python${PYTHON_VER}-venv python${PYTHON_VER}-dev
+# sudo add-apt-repository ppa:deadsnakes/ppa
+# sudo apt update
+# sudo apt install python${PYTHON_VER} python${PYTHON_VER}-venv python${PYTHON_VER}-dev
 
 rm -rf venv_sa_conv
 python${PYTHON_VER} -m venv venv_sa_conv
