2bdf9aacaec99ded47b1793216f7e03975bcdc87
Hovnatan Karapetyan <hovnatan@gmail.com>
2021-03-18 14:53:58 +0400

    Docker integration

---
+++ run_dev_env_in_container.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# docker build -t superannotate/pythonsdk-dev-env:latest -f Dockerfile_dev .
+# docker pull superannotate/pythonsdk-dev-env:latest
+
+docker run -it --rm -p 8888:8888 \
+    -v ${HOME}/.superannotate:/root/.superannotate \
+    -v $(pwd):/root/superannotate-python-sdk superannotate/pythonsdk-dev-env \
+    bash -c "pip install -e superannotate-python-sdk && jupyter lab --allow-root --NotebookApp.token='' --NotebookApp.password='' --no-browser --ip 0.0.0.0"
