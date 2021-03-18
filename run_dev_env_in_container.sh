5334ba1981c88571563620a8ad57e2cd497f22f0
Hovnatan Karapetyan <hovnatan@gmail.com>
2021-03-18 15:06:18 +0400

    Cleanup dev env container

--- run_dev_env_in_container.sh
+++ run_dev_env_in_container.sh
@@ -3,7 +3,13 @@
 # docker build -t superannotate/pythonsdk-dev-env:latest -f Dockerfile_dev .
 # docker pull superannotate/pythonsdk-dev-env:latest
 
+# docker run -it --rm -p 8888:8888 \
+#     -v ${HOME}/.superannotate:/root/.superannotate \
+#     -v $(pwd):/root/superannotate-python-sdk \
+#     superannotate/pythonsdk-dev-env \
+#     bash -c "pip install -e superannotate-python-sdk && jupyter lab --allow-root --NotebookApp.token='' --NotebookApp.password='' --no-browser --ip 0.0.0.0"
+
 docker run -it --rm -p 8888:8888 \
     -v ${HOME}/.superannotate:/root/.superannotate \
-    -v $(pwd):/root/superannotate-python-sdk superannotate/pythonsdk-dev-env \
-    bash -c "pip install -e superannotate-python-sdk && jupyter lab --allow-root --NotebookApp.token='' --NotebookApp.password='' --no-browser --ip 0.0.0.0"
+    -v $(pwd):/root/superannotate-python-sdk \
+    superannotate/pythonsdk-dev-env
