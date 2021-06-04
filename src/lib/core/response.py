10c0fbb4390f12d2725fdd7cbb1334a640d9390f
Vaghinak Basentsyan <vaghinak.basentsyan@Vaghinaks-MacBook-Pro.local>
2021-06-04 11:15:11 +0400

    Added is_valid method and response errors field

--- src/lib/core/response.py
+++ src/lib/core/response.py
@@ -2,6 +2,7 @@ class Response:
     def __init__(self, status: str = None, data: dict = None):
         self._status = status
         self._data = data
+        self._errors = []
 
     @property
     def data(self):
@@ -18,3 +19,11 @@ class Response:
     @status.setter
     def status(self, value):
         self._status = value
+
+    @property
+    def errors(self):
+        return self._errors
+
+    @errors.setter
+    def errors(self, errors: list):
+        self._errors = errors
