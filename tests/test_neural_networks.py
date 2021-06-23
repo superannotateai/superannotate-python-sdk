5e900de9d9c222328a23979863bfeff7060dec66
shab <d.shabin@gmail.com>
2021-06-23 11:20:22 +0400

    Fix tests

--- tests/test_neural_networks.py
+++ tests/test_neural_networks.py
@@ -8,35 +8,35 @@ project_name = "training"
 
 
 def test_run_training():
-    # export_path = test_root / 'consensus_benchmark' / 'consensus_test_data'
-    # if len(sa.search_projects(project_name)) != 0:
-    #     sa.delete_project(project_name)
-    # time.sleep(2)
-    #
-    # sa.create_project(project_name, "test bench", "Vector")
-    # time.sleep(2)
-    # for i in range(1, 4):
-    #     sa.create_folder(project_name, "consensus_" + str(i))
-    # time.sleep(2)
-    # sa.create_annotation_classes_from_classes_json(
-    #     project_name, export_path / 'classes' / 'classes.json'
-    # )
-    # sa.upload_images_from_folder_to_project(
-    #     project_name, export_path / "images", annotation_status="Completed"
-    # )
-    # for i in range(1, 4):
-    #     sa.upload_images_from_folder_to_project(
-    #         project_name + '/consensus_' + str(i),
-    #         export_path / "images",
-    #         annotation_status="Completed"
-    #     )
-    # sa.upload_annotations_from_folder_to_project(project_name, export_path)
-    # for i in range(1, 4):
-    #     sa.upload_annotations_from_folder_to_project(
-    #         project_name + '/consensus_' + str(i),
-    #         export_path / ('consensus_' + str(i))
-    #     )
-    # time.sleep(2)
+    export_path = test_root / 'consensus_benchmark' / 'consensus_test_data'
+    if len(sa.search_projects(project_name)) != 0:
+        sa.delete_project(project_name)
+    time.sleep(2)
+
+    sa.create_project(project_name, "test bench", "Vector")
+    time.sleep(2)
+    for i in range(1, 4):
+        sa.create_folder(project_name, "consensus_" + str(i))
+    time.sleep(2)
+    sa.create_annotation_classes_from_classes_json(
+        project_name, export_path / 'classes' / 'classes.json'
+    )
+    sa.upload_images_from_folder_to_project(
+        project_name, export_path / "images", annotation_status="Completed"
+    )
+    for i in range(1, 4):
+        sa.upload_images_from_folder_to_project(
+            project_name + '/consensus_' + str(i),
+            export_path / "images",
+            annotation_status="Completed"
+        )
+    sa.upload_annotations_from_folder_to_project(project_name, export_path)
+    for i in range(1, 4):
+        sa.upload_annotations_from_folder_to_project(
+            project_name + '/consensus_' + str(i),
+            export_path / ('consensus_' + str(i))
+        )
+    time.sleep(2)
     new_model = sa.run_training(
         "some name",
         "some desc",
