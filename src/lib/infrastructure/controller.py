744b80e8304c4895b9f0d4c2fb6a3e52de72bd85
shab <d.shabin@gmail.com>
2021-07-14 14:50:42 +0400

    remove comment

--- src/lib/infrastructure/controller.py
+++ src/lib/infrastructure/controller.py
@@ -81,8 +81,6 @@ from src.lib.infrastructure.repositories import TeamRepository
 from src.lib.infrastructure.repositories import WorkflowRepository
 from src.lib.infrastructure.services import SuperannotateBackendService
 
-# from src.lib.core.usecases import CreateAnnotationClassesJson
-
 
 class BaseController:
     def __init__(self, backend_client: SuperannotateBackendService, response: Response):
