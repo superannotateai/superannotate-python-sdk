d9efdfee4358617a52881815265185b165491170
shab <d.shabin@gmail.com>
2021-08-10 15:43:38 +0400

    Add enum

--- src/lib/app/interface/sdk_interface.py
+++ src/lib/app/interface/sdk_interface.py
@@ -14,6 +14,7 @@ from urllib.parse import urlparse
 
 import boto3
 import lib.core as constances
+from src.lib.core.enums import ImageQuality
 import pandas as pd
 import plotly.graph_objects as go
 from lib.app.annotation_helpers import add_annotation_bbox_to_json
@@ -836,10 +837,7 @@ def set_project_default_image_quality_in_editor(project, image_quality_in_editor
     :type image_quality_in_editor: str
     """
     project_name, folder_name = split_project_path(project)
-    if image_quality_in_editor == 'original':
-        image_quality_in_editor = 100
-    else:
-        image_quality_in_editor = 60
+    image_quality_in_editor = ImageQuality.get_value(image_quality_in_editor)
 
     updated = controller.set_project_settings(
         project_name=project_name,
