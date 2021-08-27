ea195439a5ea689ef7348c5fc39e3068847af3a7
Vaghinak Basentsyan <vaghinak.basentsyan@Vaghinaks-MacBook-Pro.local>
2021-08-27 12:16:20 +0400

    tod

--- src/superannotate/lib/app/helpers.py
+++ src/superannotate/lib/app/helpers.py
@@ -72,11 +72,12 @@ def get_s3_annotation_paths(folder_path, s3_bucket, annotation_paths, recursive)
     paginator = s3_client.get_paginator("list_objects_v2")
     for data in paginator.paginate(Bucket=s3_bucket, Prefix=folder_path):
         for annotation in data["Contents"]:
-            if annotation["Key"].endswith(VECTOR_ANNOTATION_POSTFIX) or annotation[
-                "Key"
-            ].endswith(PIXEL_ANNOTATION_POSTFIX):
+            if (
+                annotation["Key"].endswith(VECTOR_ANNOTATION_POSTFIX)
+                or annotation["Key"].endswith(PIXEL_ANNOTATION_POSTFIX)
+            ):
                 annotation_paths.append(annotation["Key"])
-    return annotation_paths
+    return list(set(annotation_paths))
 
 
 def reformat_metrics_json(data, name):
