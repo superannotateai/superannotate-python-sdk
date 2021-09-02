464a5ce737c2df066cfd54ed8b256109b2a52192
shab <d.shabin@gmail.com>
2021-09-02 15:23:32 +0400

    Fix test

--- tests/convertors/test_consensus.py
+++ tests/convertors/test_consensus.py
@@ -1,4 +1,5 @@
 import os
+import tempfile
 import time
 from os.path import dirname
 
@@ -38,60 +39,63 @@ class TestConsensus(BaseTestCase):
             "folderName",
             "score",
         ]
-        export_path = self.export_path
-        for i in range(1, 4):
-            sa.create_folder(self.PROJECT_NAME, "consensus_" + str(i))
-        sa.create_annotation_classes_from_classes_json(
-            self.PROJECT_NAME, self.export_path + "/classes/classes.json"
-        )
-        sa.upload_images_from_folder_to_project(
-            self.PROJECT_NAME,
-            self.export_path + "/images",
-            annotation_status="Completed",
-        )
-        for i in range(1, 4):
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            export_path = str(tmpdir)
+
+            for i in range(1, 4):
+                sa.create_folder(self.PROJECT_NAME, "consensus_" + str(i))
+            sa.create_annotation_classes_from_classes_json(
+                self.PROJECT_NAME, self.export_path + "/classes/classes.json"
+            )
             sa.upload_images_from_folder_to_project(
-                self.PROJECT_NAME + f"/{self.CONSENSUS_PREFIX}" + str(i),
+                self.PROJECT_NAME,
                 self.export_path + "/images",
                 annotation_status="Completed",
             )
-        time.sleep(2)
-        sa.upload_annotations_from_folder_to_project(
-            self.PROJECT_NAME, self.export_path
-        )
-        for i in range(1, 4):
+            for i in range(1, 4):
+                sa.upload_images_from_folder_to_project(
+                    self.PROJECT_NAME + f"/{self.CONSENSUS_PREFIX}" + str(i),
+                    self.export_path + "/images",
+                    annotation_status="Completed",
+                )
+            time.sleep(2)
             sa.upload_annotations_from_folder_to_project(
-                self.PROJECT_NAME + f"/{self.CONSENSUS_PREFIX}" + str(i),
-                self.export_path + f"/{self.CONSENSUS_PREFIX}" + str(i),
+                self.PROJECT_NAME, self.export_path
+            )
+            for i in range(1, 4):
+                sa.upload_annotations_from_folder_to_project(
+                    self.PROJECT_NAME + f"/{self.CONSENSUS_PREFIX}" + str(i),
+                    self.export_path + f"/{self.CONSENSUS_PREFIX}" + str(i),
+                )
+
+            for annot_type in annot_types:
+                res_df = sa.consensus(
+                    self.PROJECT_NAME, folder_names, annot_type=annot_type
+                )
+                # test content of projectName column
+                assert sorted(res_df["folderName"].unique()) == folder_names
+
+                # test structure of resulting DataFrame
+                assert sorted(res_df.columns) == sorted(df_column_names)
+
+                # test lower bound of the score
+                assert (res_df["score"] >= 0).all()
+
+                # test upper bound of the score
+                assert (res_df["score"] <= 1).all()
+
+            image_names = [
+                "bonn_000000_000019_leftImg8bit.png",
+                "bielefeld_000000_000321_leftImg8bit.png",
+            ]
+
+            # test filtering images with given image names list
+            res_images = sa.consensus(
+                self.PROJECT_NAME,
+                folder_names,
+                export_root=export_path,
+                image_list=image_names,
             )
 
-        for annot_type in annot_types:
-            res_df = sa.consensus(
-                self.PROJECT_NAME, folder_names, annot_type=annot_type
-            )
-            # test content of projectName column
-            assert sorted(res_df["folderName"].unique()) == folder_names
-
-            # test structure of resulting DataFrame
-            assert sorted(res_df.columns) == sorted(df_column_names)
-
-            # test lower bound of the score
-            assert (res_df["score"] >= 0).all()
-
-            # test upper bound of the score
-            assert (res_df["score"] <= 1).all()
-
-        image_names = [
-            "bonn_000000_000019_leftImg8bit.png",
-            "bielefeld_000000_000321_leftImg8bit.png",
-        ]
-
-        # test filtering images with given image names list
-        res_images = sa.consensus(
-            self.PROJECT_NAME,
-            folder_names,
-            export_root=export_path,
-            image_list=image_names,
-        )
-
-        assert sorted(res_images["imageName"].unique()) == sorted(image_names)
+            assert sorted(res_images["imageName"].unique()) == sorted(image_names)
