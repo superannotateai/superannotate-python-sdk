c0c245e540a6804579adf9d36523485b891df40c
shab <d.shabin@gmail.com>
2021-09-22 11:02:01 +0400

    Fix deperecated funcs

--- tests/integration/test_depricated_functions_document.py
+++ tests/integration/test_depricated_functions_document.py
@@ -1,10 +1,12 @@
 import os
 from os.path import dirname
 import src.superannotate as sa
+from src.superannotate import AppException
 from tests.integration.base import BaseTestCase
 from src.superannotate.lib.core import LIMITED_FUNCTIONS
-from src.superannotate.lib.core import DEPRICATED_DOCUMENT_VIDEO_MESSAGE
 from src.superannotate.lib.core import ProjectType
+from src.superannotate.lib.core import DEPRICATED_DOCUMENT_VIDEO_MESSAGE
+
 
 class TestDepricatedFunctionsDocument(BaseTestCase):
     PROJECT_NAME = "document proj 11"
@@ -19,7 +21,6 @@ class TestDepricatedFunctionsDocument(BaseTestCase):
     PROJECT_TYPE_2 = "Vector"
     EXCEPTION_MESSAGE = LIMITED_FUNCTIONS[ProjectType.DOCUMENT.value]
     EXCEPTION_MESSAGE_DOCUMENT_VIDEO = DEPRICATED_DOCUMENT_VIDEO_MESSAGE
-
 
     def setUp(self, *args, **kwargs):
         self.tearDown()
@@ -57,238 +58,165 @@ class TestDepricatedFunctionsDocument(BaseTestCase):
     def image_path(self):
         return f'{self.folder_path}/example_image_1.jpg'
 
-
-    def test_depricated_functions(self):
+    def test_deprecated_functions(self):
         _, _, _ = sa.attach_document_urls_to_project(
             self.PROJECT_NAME,
             os.path.join(dirname(dirname(__file__)), self.PATH_TO_URLS),
         )
-        msg = ""
         try:
-            msg = ""
             sa.upload_images_from_folder_to_project(self.PROJECT_NAME,"some")
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.get_image_metadata(self.PROJECT_NAME,"some")
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.search_images(self.PROJECT_NAME)
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.upload_images_to_project(self.PROJECT_NAME,["some"])
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
-            sa.upload_annotations_from_folder_to_project(self.PROJECT_NAME,"some")
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+            sa.upload_annotations_from_folder_to_project(self.PROJECT_NAME,"some",self.annotation_path)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.upload_image_annotations(self.PROJECT_NAME,"some",self.annotation_path)
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.download_image(self.PROJECT_NAME,self.UPLOAD_IMAGE_NAME,'./')
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.download_image_annotations(self.PROJECT_NAME,self.UPLOAD_IMAGE_NAME,"./")
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.get_image_annotations(self.PROJECT_NAME,self.UPLOAD_IMAGE_NAME)
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.set_image_annotation_status(self.PROJECT_NAME,self.UPLOAD_IMAGE_NAME,"Completed")
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.attach_image_urls_to_project(
                 self.PROJECT_NAME,
                 os.path.join(dirname(dirname(__file__)), self.PATH_TO_URLS),
             )
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.clone_project(self.PROJECT_NAME_2,self.PROJECT_NAME)
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.copy_image(self.PROJECT_NAME,self.UPLOAD_IMAGE_NAME,self.PROJECT_NAME_2)
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.upload_images_to_project(self.PROJECT_NAME,[self.image_path,])
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.upload_video_to_project(self.PROJECT_NAME,"some path")
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.add_annotation_bbox_to_image(self.PROJECT_NAME, self.UPLOAD_IMAGE_NAME, [1,2,3,4] , "some class")
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.delete_images(self.PROJECT_NAME, [self.UPLOAD_IMAGE_NAME])
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.get_project_image_count(self.PROJECT_NAME)
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.set_project_workflow(self.PROJECT_NAME, [{}])
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.upload_preannotations_from_folder_to_project(self.PROJECT_NAME, self.folder_path)
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.add_annotation_comment_to_image(self.PROJECT_NAME, self.UPLOAD_IMAGE_NAME,"some comment",[1,2],"some user")
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.add_annotation_point_to_image(self.PROJECT_NAME, self.UPLOAD_IMAGE_NAME,[1,2],"some class")
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.consensus(self.PROJECT_NAME, ["some"], self.video_export_path)
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE_DOCUMENT_VIDEO, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE_DOCUMENT_VIDEO, str(e))
         try:
-            msg = ""
             sa.copy_images(self.PROJECT_NAME, [self.UPLOAD_IMAGE_NAME], self.PROJECT_NAME_2)
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.get_project_workflow(self.PROJECT_NAME)
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.move_image(self.PROJECT_NAME,self.UPLOAD_IMAGE_NAME, self.PROJECT_NAME_2)
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.move_images(self.PROJECT_NAME,[self.UPLOAD_IMAGE_NAME], self.PROJECT_NAME_2)
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.set_images_annotation_statuses(self.PROJECT_NAME,"Completed",[self.UPLOAD_IMAGE_NAME])
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.class_distribution(self.video_export_path, [self.PROJECT_NAME])
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE_DOCUMENT_VIDEO, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE_DOCUMENT_VIDEO, str(e))
         try:
-            msg = ""
             sa.convert_project_type(self.video_export_path,"./")
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE_DOCUMENT_VIDEO, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE_DOCUMENT_VIDEO, str(e))
         try:
-            msg = ""
             sa.prepare_export(self.PROJECT_NAME,include_fuse=True,only_pinned=True)
-        except Exception as e:
-            msg = str(e)
-        self.assertIn("Include fuse functionality is not supported", msg)
+        except AppException as e:
+            self.assertIn("Include fuse functionality is not supported", str(e))
         try:
-            msg = ""
             sa.attach_video_urls_to_project(
                 self.PROJECT_NAME,
                 os.path.join(dirname(dirname(__file__)), self.PATH_TO_URLS),
             )
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.benchmark(self.PROJECT_NAME, "some", ["some folder1"])
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.upload_videos_from_folder_to_project(self.PROJECT_NAME, self.folder_path)
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.assign_images(self.PROJECT_NAME,[self.UPLOAD_IMAGE_NAME],"some user")
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
         try:
-            msg = ""
             sa.export_annotation(
-                "input_dir", "fromSuperAnnotate/panoptic_test", "COCO", "panoptic_test", "Document",
+                "input_dir", "fromSuperAnnotate/panoptic_test", "COCO", "panoptic_test", self.PROJECT_TYPE,
                 "panoptic_segmentation"
             )
-        except Exception as e:
-            msg = str(e)
-        self.assertIn(self.EXCEPTION_MESSAGE, msg)
+        except AppException as e:
+            self.assertIn(self.EXCEPTION_MESSAGE, str(e))
+
         # TODO: image quality error
         # try:
         #     msg = ""
