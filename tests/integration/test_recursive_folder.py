8c1182d44c38318e834c072a2051730caa4cbe3b
shab <d.shabin@gmail.com>
2021-09-28 10:17:38 +0400

    Add tests

--- tests/integration/test_recursive_folder.py
+++ tests/integration/test_recursive_folder.py
@@ -304,3 +304,27 @@ class TestRecursiveFolder(BaseTestCase):
             self.PROJECT_NAME, self.folder_path, recursive_subfolders=False
         )
         self.assertEqual(len(sa.search_images(self.PROJECT_NAME)), 1)
+
+
+class TestRecursiveFolderPixel(BaseTestCase):
+    PROJECT_NAME = "test_recursive_pixel"
+    PROJECT_DESCRIPTION = "Desc"
+    PROJECT_TYPE = "Pixel"
+    S3_FOLDER_PATH = "sample_project_pixel"
+    TEST_FOLDER_PATH = "data_set/sample_project_pixel"
+
+    @property
+    def folder_path(self):
+        return os.path.join(dirname(dirname(__file__)), self.TEST_FOLDER_PATH)
+
+    def test_recursive_annotation_upload_pixel(self):
+        sa.upload_images_from_folder_to_project(
+            self.PROJECT_NAME, self.folder_path, recursive_subfolders=False
+        )
+        uploaded_annotations,_,_ = sa.upload_annotations_from_folder_to_project(self.PROJECT_NAME, self.S3_FOLDER_PATH,
+                                                                from_s3_bucket="superannotate-python-sdk-test",
+                                                                recursive_subfolders=False)
+        self.assertEqual(len(uploaded_annotations), 3)
+
+
+
