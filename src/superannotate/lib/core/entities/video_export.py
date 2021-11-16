36f6358aedec1ab17b4e48b27e1de30b5eef8abd
Vaghinak Basentsyan <vaghinak.basentsyan@Vaghinaks-MacBook-Pro.local>
2021-11-16 15:12:29 +0400

    Fixed 508

--- src/superannotate/lib/core/entities/video_export.py
+++ src/superannotate/lib/core/entities/video_export.py
@@ -11,6 +11,7 @@ from lib.core.entities.utils import BboxPoints
 from lib.core.entities.utils import MetadataBase
 from lib.core.entities.utils import NotEmptyStr
 from lib.core.entities.utils import PointLabels
+from lib.core.entities.utils import INVALID_DICT_MESSAGE
 from lib.core.entities.utils import Tag
 from pydantic import conlist
 from pydantic import Field
@@ -96,12 +97,12 @@ class VideoInstance(BaseModel):
     @classmethod
     def return_action(cls, values):
         try:
-            instance_type = values["meta"]["type"]
-        except KeyError:
-            raise ValidationError(
-                [ErrorWrapper(ValueError("meta.field required"), "type")], cls
-            )
-        try:
+            try:
+                instance_type = values["meta"]["type"]
+            except KeyError:
+                raise ValidationError(
+                    [ErrorWrapper(ValueError("meta.field required"), "type")], cls
+                )
             return INSTANCES[instance_type](**values)
         except KeyError:
             raise ValidationError(
@@ -115,6 +116,8 @@ class VideoInstance(BaseModel):
                 ],
                 cls,
             )
+        except TypeError as e:
+            raise TypeError(INVALID_DICT_MESSAGE) from e
 
 
 class VideoAnnotation(BaseModel):
