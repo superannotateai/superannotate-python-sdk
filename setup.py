56880603056b133bbfeaebe49d3e7a73b1bea24b
Vaghinak Basentsyan <vaghinak@superannotate.com>
2022-07-19 10:19:39 +0400

    changed setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
-from packaging.version import parse
+import os
+import re
+
 from setuptools import find_packages, setup
 
 
-with open('src/superannotate/version.py') as f:
-    version = f.read().rstrip()[15:-1]
+def get_version():
+    init = open(os.path.join(os.path.dirname(__file__), 'src', 'superannotate', '__init__.py')).read()
+    version_re = re.compile(r'''__version__ = ["']((\d+)\.(\d+)\.(\d+)((dev(\d+))?(b(\d+))?))['"]''')
+    return version_re.search(init).group(1)
 
-requirements_path = "requirements_{}.txt".format('dev' if parse(version).is_prerelease else 'prod')
+
+sdk_version = get_version()
+
+
+requirements_path = "requirements_{}.txt".format('dev' if 'dev' in sdk_version else 'prod')
+
 requirements = []
 
 with open("requirements.txt") as f:
@@ -23,14 +32,13 @@ readme = "\n".join(readme.split('\n')[2:])
 
 setup(
     name='superannotate',
-    version=version,
+    version=sdk_version,
     package_dir={"": "src"},
     package_data={"superannotate": ["logging.conf"]},
     packages=find_packages(where="src"),
     description='Python SDK to SuperAnnotate platform',
     license='MIT',
     author='SuperAnnotate AI',
-    author_email='vaghinak@superannotate.com',
     url='https://github.com/superannotateai/superannotate-python-sdk',
     long_description=readme,
     long_description_content_type='text/markdown',
@@ -40,5 +48,17 @@ setup(
     entry_points={
         'console_scripts': ['superannotatecli = superannotate.lib.app.bin.superannotate:main']
     },
-    python_requires='>=3.6'
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    project_urls={
+        'Documentation': 'https://superannotate.readthedocs.io/en/stable/',
+    },
+    python_requires='>=3.7'
 )
