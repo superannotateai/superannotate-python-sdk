d9df7dc017709e50e040ea6bee72c176860cc62c
Vaghinak Basentsyan <vaghinak@superannotate.com>
2022-10-14 14:39:09 +0400

    Fix logs

--- src/superannotate/lib/core/usecases/projects.py
+++ src/superannotate/lib/core/usecases/projects.py
@@ -914,7 +914,7 @@ class AddContributorsToProject(BaseUserBasedUseCase):
         emails = list(set(self._emails))
         len_unique, len_provided = len(emails), len(self._emails)
         if len_unique < len_provided:
-            f"Dropping duplicates. Found {len_unique}/{len_provided} unique users."
+            logger.info(f"Dropping duplicates. Found {len_unique}/{len_provided} unique users.")
         self._emails = emails
 
     def execute(self):
