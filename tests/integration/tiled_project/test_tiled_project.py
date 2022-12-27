97f46dc7cf5f111f1ccd710188d5645de81cd025
Vaghinak Basentsyan <vaghinak@superannotate.com>
2022-12-27 10:08:28 +0400

    test fix

---
+++ tests/integration/tiled_project/test_tiled_project.py
@@ -0,0 +1,14 @@
+from src.superannotate import SAClient
+from tests.integration.base import BaseTestCase
+
+sa = SAClient()
+
+
+class TestTiledProject(BaseTestCase):
+    PROJECT_NAME = "TestGetIntegrations"
+    PROJECT_DESCRIPTION = "desc"
+    PROJECT_TYPE = "Tiled"
+
+    def test_get_metadata(self):
+        self._attach_items()
+        assert all([i['metadata']['name'] for i in sa.get_annotations(self.PROJECT_NAME)])
