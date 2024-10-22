55360dd1ad4ebf5a6aad4785d85c083bb9c6188e
Narek Mkhitaryan <narekm@Nareks-MacBook-Pro-2.local>
2024-10-22 20:02:43 +0400

    updated item tests

--- tests/integration/items/test_move_items.py
+++ tests/integration/items/test_move_items.py
@@ -17,6 +17,17 @@ class TestMoveItems(BaseTestCase):
     FOLDER_2 = "folder_2"
     CSV_PATH = "data_set/attach_urls.csv"
 
+    Attachment = [
+        {
+            "url": "https://drive.google.com/uc?export=download&id=1vwfCpTzcjxoEA4hhDxqapPOVvLVeS7ZS",
+            "name": IMAGE_NAME,
+        },
+        {
+            "url": "https://drive.google.com/uc?export=download&id=1vwfCpTzcjxoEA4hhDxqapPOVvLVeS7Zw",
+            "name": IMAGE_NAME_2,
+        },
+    ]
+
     @property
     def scv_path(self):
         return os.path.join(Path(__file__).parent.parent.parent, self.CSV_PATH)
@@ -46,21 +57,11 @@ class TestMoveItems(BaseTestCase):
         assert len(sa.search_items(f"{self.PROJECT_NAME}/{self.FOLDER_2}")) == 7
         assert len(sa.search_items(f"{self.PROJECT_NAME}/{self.FOLDER_1}")) == 0
 
-    def test_move_items_from_folder_with_replace_strategy(self):
-        attachment = [
-            {
-                "url": "https://drive.google.com/uc?export=download&id=1vwfCpTzcjxoEA4hhDxqapPOVvLVeS7ZS",
-                "name": self.IMAGE_NAME,
-            },
-            {
-                "url": "https://drive.google.com/uc?export=download&id=1vwfCpTzcjxoEA4hhDxqapPOVvLVeS7Zw",
-                "name": self.IMAGE_NAME_2,
-            },
-        ]
+    def test_move_items_from_folder_with_replace(self):
         sa.create_folder(self.PROJECT_NAME, self.FOLDER_1)
         sa.create_folder(self.PROJECT_NAME, self.FOLDER_2)
         uploaded, _, _ = sa.attach_items(
-            f"{self.PROJECT_NAME}/{self.FOLDER_1}", attachment
+            f"{self.PROJECT_NAME}/{self.FOLDER_1}", self.Attachment
         )
         assert len(uploaded) == 2
         sa.set_approval_statuses(
@@ -75,7 +76,7 @@ class TestMoveItems(BaseTestCase):
         )
 
         uploaded_2, _, _ = sa.attach_items(
-            f"{self.PROJECT_NAME}/{self.FOLDER_2}", attachment
+            f"{self.PROJECT_NAME}/{self.FOLDER_2}", self.Attachment
         )
         assert len(uploaded_2) == 2
         folder_2_items = sa.search_items(f"{self.PROJECT_NAME}/{self.FOLDER_2}")
@@ -96,3 +97,64 @@ class TestMoveItems(BaseTestCase):
         folder_2_items = sa.search_items(f"{self.PROJECT_NAME}/{self.FOLDER_2}")
         assert folder_2_items[0]["annotation_status"] == "Completed"
         assert folder_2_items[0]["approval_status"] == "Approved"
+
+    def test_move_items_from_folder_with_replace_annotations_only(self):
+        sa.create_folder(self.PROJECT_NAME, self.FOLDER_1)
+        sa.create_folder(self.PROJECT_NAME, self.FOLDER_2)
+        uploaded, _, _ = sa.attach_items(
+            f"{self.PROJECT_NAME}/{self.FOLDER_1}", self.Attachment
+        )
+        assert len(uploaded) == 2
+        sa.set_approval_statuses(
+            f"{self.PROJECT_NAME}/{self.FOLDER_1}",
+            "Approved",
+            items=[self.IMAGE_NAME, self.IMAGE_NAME_2],
+        )
+        sa.set_annotation_statuses(
+            f"{self.PROJECT_NAME}/{self.FOLDER_1}",
+            "Completed",
+            items=[self.IMAGE_NAME, self.IMAGE_NAME_2],
+        )
+
+        uploaded_2, _, _ = sa.attach_items(
+            f"{self.PROJECT_NAME}/{self.FOLDER_2}", self.Attachment
+        )
+        assert len(uploaded_2) == 2
+        folder_2_items = sa.search_items(f"{self.PROJECT_NAME}/{self.FOLDER_2}")
+        assert folder_2_items[0]["annotation_status"] == "NotStarted"
+        assert not folder_2_items[0]["approval_status"]
+
+        skipped_items = sa.move_items(
+            f"{self.PROJECT_NAME}/{self.FOLDER_1}",
+            f"{self.PROJECT_NAME}/{self.FOLDER_2}",
+            duplicate_strategy="replace_annotations_only",
+        )
+        assert len(skipped_items) == 0
+        folder_1_items = sa.search_items(f"{self.PROJECT_NAME}/{self.FOLDER_1}")
+        folder_2_items = sa.search_items(f"{self.PROJECT_NAME}/{self.FOLDER_2}")
+        assert len(folder_1_items) == 0
+        assert len(folder_2_items) == 2
+
+        folder_2_items = sa.search_items(f"{self.PROJECT_NAME}/{self.FOLDER_2}")
+        assert folder_2_items[0]["annotation_status"] == "NotStarted"
+        assert not folder_2_items[0]["approval_status"]
+
+    def test_move_items_from_folder_with_skip(self):
+        sa.create_folder(self.PROJECT_NAME, self.FOLDER_1)
+        sa.create_folder(self.PROJECT_NAME, self.FOLDER_2)
+        uploaded, _, _ = sa.attach_items(
+            f"{self.PROJECT_NAME}/{self.FOLDER_1}", self.Attachment
+        )
+        assert len(uploaded) == 2
+
+        uploaded_2, _, _ = sa.attach_items(
+            f"{self.PROJECT_NAME}/{self.FOLDER_2}", self.Attachment
+        )
+        assert len(uploaded_2) == 2
+
+        skipped_items = sa.move_items(
+            f"{self.PROJECT_NAME}/{self.FOLDER_1}",
+            f"{self.PROJECT_NAME}/{self.FOLDER_2}",
+            duplicate_strategy="skip",
+        )
+        assert len(skipped_items) == 2
