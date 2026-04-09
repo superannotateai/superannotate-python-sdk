90b934d966b36f0d66a356304f485ffd3ea7db15
Vaghinak Basentsyan <vaghinak@superannotate.com>
2026-04-09 18:45:08 +0400

    Fix saqul query pagination

--- src/superannotate/lib/infrastructure/services/explore.py
+++ src/superannotate/lib/infrastructure/services/explore.py
@@ -165,43 +165,33 @@ class ExploreService(BaseExploreService):
         query: str = None,
         subset_id: int = None,
     ) -> ServiceResponse:
-
-        params = {
-            "project_id": project.id,
-            "includeFolderNames": True,
-        }
+        params = {"project_id": project.id, "includeFolderNames": True}
         if folder:
             params["folder_id"] = folder.id
         if subset_id:
             params["subset_id"] = subset_id
+
         data = {"image_index": 0}
         if query:
             data["query"] = query
+
         items = []
-        response = None
+        url = urljoin(self.explore_service_url, self.URL_SAQUL_QUERY)
+
         for _ in range(0, self.MAX_ITEMS_COUNT, self.SAQUL_CHUNK_SIZE):
-            response = self.client.request(
-                urljoin(self.explore_service_url, self.URL_SAQUL_QUERY),
-                "post",
-                params=params,
-                data=data,
-            )
+            response = self.client.request(url, "post", params=params, data=data)
             if not response.ok:
-                break
-            response_items = response.data
-            items.extend(response_items)
-            if len(response_items) < self.SAQUL_CHUNK_SIZE:
+                return ServiceResponse(
+                    status=response.status_code,
+                    res_data=items,
+                    res_error=response.error,
+                )
+            items.extend(response.data)
+            if len(response.data) < self.SAQUL_CHUNK_SIZE:
                 break
             data["image_index"] += self.SAQUL_CHUNK_SIZE
 
-        if response:
-            response = ServiceResponse(status=response.status_code, res_data=items)
-            if not response.ok:
-                response.set_error(response.error)
-                response = ServiceResponse(status=response.status_code, res_data=items)
-        else:
-            response = ServiceResponse(status=200, res_data=[])
-        return response
+        return ServiceResponse(status=200, res_data=items)
 
     def query_item_count(
         self,
